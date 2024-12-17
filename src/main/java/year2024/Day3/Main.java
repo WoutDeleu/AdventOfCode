9009adf62c13f1828e542ebb1aa1a152f090acc5
WDELEU <WDELEU@volvocars.com>
2024-12-17 10:27:34 +0100

    Generate 2024

---
+++ src/main/java/year2024/Day3/Main.java
@@ -0,0 +1,11 @@
+    // package year2023.Day3;
+
+    // import java.io.File;
+    // import java.io.FileNotFoundException;
+    // public class Main {
+    //     public static void main(String[] args) throws FileNotFoundException {
+    //         int[] cards = read("./src/main/java/year2023/Day3/input");
+    //         int score = solve_pt1(cards);
+    //         System.out.println("score: " + score);
+    //     }
+    // }
---
+++ src/test/java/year2024/Day6/MainTest.java
@@ -0,0 +1,20 @@
+    // package year2023.Day6;
+
+    // import java.io.File;
+    // import java.io.FileNotFoundException;
+    // public class AlmanacFoodProductionSolverTest {
+    //
+    //     @Test
+    //     public void testMain() throws FileNotFoundException {
+    //         int[] cards = read("./src/test/java/year2023/Day6/input");
+    //         int score = solve_pt1(cards);
+    //         assert score == 35;
+    //     }
+    //     @Test
+    //     public void testMainV2() throws FileNotFoundException {
+    //         int[] cards = read("./src/test/java/year2023/Day6/input");
+    //         int score = solve_pt1(cards);
+    //         assert score == 35;
+    //     }
+    // }
+
