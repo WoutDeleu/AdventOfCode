135c9aafa54571045d2c6111044ee24ee0551560
WDELEU <WDELEU@volvocars.com>
2024-12-17 10:26:35 +0100

    Refactor codebase

--- src/main/java/year2023/Day14/Main.java
+++ src/main/java/year2023/Day14/Main.java
@@ -5,7 +5,7 @@
     // public class MapSolver {
     //     public static void main(String[] args) throws FileNotFoundException {
     //         int[] cards = read("./src/main/java/year2023/Day14/input");
-    //         int score = solve(cards);
+    //         int score = solve_pt1(cards);
     //         System.out.println("score: " + score);
     //     }
     // }
--- src/test/java/year2023/Day13/MainTest.java
+++ src/test/java/year2023/Day13/MainTest.java
@@ -7,13 +7,13 @@
     //     @Test
     //     public void testMain() throws FileNotFoundException {
     //         int[] cards = read("./src/test/java/year2023/Day13/input");
-    //         int score = solve(cards);
+    //         int score = solve_pt1(cards);
     //         assert score == 35;
     //     }
     //     @Test
     //     public void testMainV2() throws FileNotFoundException {
     //         int[] cards = read("./src/test/java/year2023/Day13/input");
-    //         int score = solve(cards);
+    //         int score = solve_pt1(cards);
     //         assert score == 35;
     //     }
     // }
