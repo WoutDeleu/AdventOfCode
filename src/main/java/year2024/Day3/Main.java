f5ab5bc9e22e031229808884e83cb97c70c18315
WDELEU <WDELEU@volvocars.com>
2024-12-17 15:15:26 +0100

    Day 3

--- src/main/java/year2024/Day3/Main.java
+++ src/main/java/year2024/Day3/Main.java
@@ -2,8 +2,9 @@ package year2024.Day3;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
   public static void main(String[] args) throws FileNotFoundException {
@@ -16,16 +17,57 @@ public class Main {
   }
 
 
-  public int solve_pt1(List<String> input) {
-    return 0;
+  public int solve_pt2(String input) {
+    Pattern pattern = Pattern.compile("mul\\(\\d+,\\d+\\)|do\\(\\)|don't\\(\\)");
+    Matcher matcher = pattern.matcher(input);
+
+    int sum = 0;
+    boolean enabled = true;
+    while (matcher.find()) {
+      String foundMatch = matcher.group();
+
+      if (foundMatch.equals("do()")) {
+        enabled = true;
+      } else if (foundMatch.equals("don't()")) {
+        enabled = false;
+      }
+
+      if (enabled && foundMatch.startsWith("mul")) {
+        // Replaces everything that is not a digit or a comma
+        String[] numbers = foundMatch.replaceAll("[^\\d+,]", "").split(",");
+        int num1 = Integer.parseInt(numbers[0]);
+        int num2 = Integer.parseInt(numbers[1]);
+        sum += num1 * num2;
+      }
+    }
+    return sum;
   }
 
-  public int solve_pt2(List<String> input) {
-    return 0;
+  public int solve_pt1(String input) {
+    Pattern pattern = Pattern.compile("mul\\(\\d+,\\d+\\)|do\\(\\)|don't\\(\\)");
+    Matcher matcher = pattern.matcher(input);
+
+    int sum = 0;
+    while (matcher.find()) {
+      String foundMatch = matcher.group();
+
+      if (foundMatch.startsWith("mul")) {
+        // Replaces everything that is not a digit or a comma
+        String[] numbers = foundMatch.replaceAll("[^\\d+,]", "").split(",");
+        int num1 = Integer.parseInt(numbers[0]);
+        int num2 = Integer.parseInt(numbers[1]);
+        sum += num1 * num2;
+      }
+    }
+    return sum;
   }
 
-  public List<String> read(String s) throws FileNotFoundException {
+  public String read(String s) throws FileNotFoundException {
     var sc = new Scanner(new File(s));
-    return List.of();
+    StringBuilder line = new StringBuilder();
+    while (sc.hasNextLine()) {
+      line.append(sc.nextLine());
+    }
+    return line.toString();
   }
 }
--- src/test/java/year2024/Day3/MainTest.java
+++ src/test/java/year2024/Day3/MainTest.java
@@ -1,24 +1,24 @@
-  package year2024.Day3;
+package year2024.Day3;
 
-  import org.junit.Test;
+import org.junit.Test;
 
-  import java.io.FileNotFoundException;
+import java.io.FileNotFoundException;
 
 
-  public class MainTest {
-    @Test
-    public void part1() throws FileNotFoundException {
-      var main = new Main();
-      var input = main.read("./src/test/java/year2024/Day3/input");
-      int solution = main.solve_pt1(input);
-      assert solution == 1;
-    }
-
-    @Test
-    public void part2() throws FileNotFoundException {
-      var main = new Main();
-      var input = main.read("./src/test/java/year2024/Day3/input");
-      assert main.solve_pt2(input) == 1;
-    }
+public class MainTest {
+  @Test
+  public void part1() throws FileNotFoundException {
+    var main = new Main();
+    var input = main.read("./src/test/java/year2024/Day3/input");
+    int solution = main.solve_pt1(input);
+    assert solution == 161;
   }
+
+  @Test
+  public void part2() throws FileNotFoundException {
+    var main = new Main();
+    var input = main.read("./src/test/java/year2024/Day3/input");
+    assert main.solve_pt2(input) == 1;
+  }
+}
 
