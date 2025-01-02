2c7f850dc8fb2e5a18c601a966fa338f3d09db9d
woutdeleu <wout.deleu@gmail.com>
2025-01-02 14:43:37 +0100

    New File generator

---
+++ src/test/java/year2024/Day23/MainTest.java
@@ -0,0 +1,27 @@
+  package year2024.Day23;
+
+  import org.junit.Test;
+
+  import java.io.FileNotFoundException;
+
+
+  public class MainTest {
+    @Test
+    public void part1() throws FileNotFoundException {
+      var main = new Main();
+      var input = main.read("./src/test/java/year2024/Day23/input");
+      int solution = main.solve_pt1(input);
+      System.out.println("Solution: " + solution);
+      assert solution == 1;
+    }
+
+    @Test
+    public void part2() throws FileNotFoundException {
+      var main = new Main();
+      var input = main.read("./src/test/java/year2024/Day23/input");
+      int solution = main.solve_pt2(input);
+      System.out.println("Solution: " + solution);
+      assert solution == 1;
+    }
+  }
+
