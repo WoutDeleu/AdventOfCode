88a3bb0dbe5c5edd0dd3af033ae4d996c0db5264
woutdeleu <wout.deleu@gmail.com>
2025-07-16 12:32:51 +0200

    Day 7 - solved

--- src/main/java/year2024/Day7/Main.java
+++ src/main/java/year2024/Day7/Main.java
@@ -2,30 +2,124 @@ package year2024.Day7;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) throws FileNotFoundException {
     var input = new Main().read("./src/main/java/year2024/Day7/input");
-    int score = new Main().solve_pt1(input);
+    long score = new Main().solve_pt1(input);
     System.out.println("pt1: " + score);
 
-    int score2 = new Main().solve_pt2(input);
+    long score2 = new Main().solve_pt2(input);
     System.out.println("pt2: " + score2);
   }
 
-
-  public int solve_pt1(int[] input) {
-    return 0;
+  private static boolean isFirstCall(long tempSum) {
+    return tempSum == 0;
   }
 
-  public int solve_pt2(int[] input) {
-    return 0;
+  public long solve_pt1(List<Equation> equations) {
+    long result = 0;
+    for (Equation equation : equations) {
+      long desiredSum = equation.sum();
+      var args = equation.args();
+      long tempSum = 0;
+
+      if (isPossible(desiredSum, args, tempSum, false)) {
+        result += equation.sum();
+      }
+    }
+    return result;
   }
 
-  public int[] read(String s) throws FileNotFoundException {
+  private boolean isPossible(long desiredSum, Queue<Long> args, long tempSum, boolean pt2) {
+    // deep copy of args to avoid modifying the original queue
+    var currentQueue = new LinkedList<>(args);
+
+    // stop condition for recursion
+    if (currentQueue.isEmpty()) {
+      return tempSum == desiredSum;
+    }
+
+    // pruning condition
+    if (tempSum > desiredSum) {
+      return false;
+    }
+
+    long nextArg = currentQueue.poll();
+
+    ///  ADDITION
+    tempSum += nextArg;
+    if (isPossible(desiredSum, currentQueue, tempSum, pt2)) {
+      return true;
+    }
+    // recursion
+    tempSum -= nextArg;
+
+    // first element -> no need to try multiplication
+    if (isFirstCall(tempSum)) {
+      return false;
+    }
+
+    /// MULTIPLICATION
+    tempSum *= nextArg;
+    if (isPossible(desiredSum, currentQueue, tempSum, pt2)) {
+      return true;
+    }
+    tempSum /= nextArg;
+
+    // check if concatenation is allowed
+    if (!pt2) {
+      return false;
+    }
+
+    ///  CONCATENATION
+    tempSum = concatenate(tempSum, nextArg);
+    return isPossible(desiredSum, currentQueue, tempSum, pt2);
+  }
+
+  private long concatenate(long tempSum, long nextArg) {
+    String tempSumStr = String.valueOf(tempSum);
+    String nextArgStr = String.valueOf(nextArg);
+    return Long.parseLong(tempSumStr + nextArgStr);
+  }
+
+  public long solve_pt2(List<Equation> equations) {
+    long result = 0;
+    for (Equation equation : equations) {
+      long desiredSum = equation.sum();
+      var args = equation.args();
+      long tempSum = 0;
+
+      if (isPossible(desiredSum, args, tempSum, true)) {
+        result += equation.sum();
+      }
+    }
+    return result;
+  }
+
+  public List<Equation> read(String s) throws FileNotFoundException {
+    List<Equation> result = new ArrayList<>();
     var sc = new Scanner(new File(s));
-    return null;
+    while (sc.hasNextLine()) {
+      String line = sc.nextLine();
+      String[] parts = line.split(":");
+      Long sum = Long.parseLong(parts[0]);
+      var args =
+          Arrays.stream(parts[1].split(" "))
+              .filter(string -> !string.isBlank())
+              .map(Long::parseLong)
+              .toList();
+      result.add(new Equation(sum, new LinkedList<>(args)));
+    }
+
+    return result;
   }
+
+  public record Equation(Long sum, Queue<Long> args) {}
 }
--- src/test/java/year2024/Day7/MainTest.java
+++ src/test/java/year2024/Day7/MainTest.java
@@ -10,18 +10,18 @@
     public void part1() throws FileNotFoundException {
       var main = new Main();
       var input = main.read("./src/test/java/year2024/Day7/input");
-      int solution = main.solve_pt1(input);
+      var solution = main.solve_pt1(input);
       System.out.println("Solution: " + solution);
-      assert solution == 1;
+      assert solution == 3749;
     }
 
     @Test
     public void part2() throws FileNotFoundException {
       var main = new Main();
       var input = main.read("./src/test/java/year2024/Day7/input");
-      int solution = main.solve_pt2(input);
+      var solution = main.solve_pt2(input);
       System.out.println("Solution: " + solution);
-      assert solution == 1;
+      assert solution == 11387;
     }
   }
 
