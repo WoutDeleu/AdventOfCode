4bcb7e60a3d8d458bf36a789b50d6c271a609049
woutdeleu <wout.deleu@gmail.com>
2025-07-17 16:48:57 +0200

    Day 11 - pt1 solved

--- src/main/java/year2024/Day11/Main.java
+++ src/main/java/year2024/Day11/Main.java
@@ -2,30 +2,75 @@ package year2024.Day11;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+  public static int BLINKING_COUNT = 25;
+
   public static void main(String[] args) throws FileNotFoundException {
     var input = new Main().read("./src/main/java/year2024/Day11/input");
-    int score = new Main().solve_pt1(input);
+    int score = new Main().solve(input);
     System.out.println("pt1: " + score);
 
-    int score2 = new Main().solve_pt2(input);
+    BLINKING_COUNT = 75;
+    int score2 = new Main().solve(input);
     System.out.println("pt2: " + score2);
   }
 
-
-  public int solve_pt1(int[] input) {
-    return 0;
+  public int solve(List<Long> stones) {
+    for (int i = 0; i < BLINKING_COUNT; i++) {
+      stones = changeStones(stones);
+    }
+    return stones.size();
   }
 
-  public int solve_pt2(int[] input) {
-    return 0;
+
+  private boolean hasEvenNumberOfDigits(Long l) {
+    return l.toString().length() % 2 == 0;
   }
 
-  public int[] read(String s) throws FileNotFoundException {
+  private List<Long> changeStones(List<Long> stones) {
+    List<Long> newStones = new ArrayList<>();
+    for (int i = 0; i < stones.size(); i++) {
+      Long stone = stones.get(i);
+      if (stone.equals(0L)) {
+        newStones.add(1L);
+      } else if (hasEvenNumberOfDigits(stone)) {
+        Long[] split = splitLongDigitsInTwo(stone);
+        newStones.add(split[0]);
+        newStones.add(split[1]);
+      } else {
+        newStones.add(stone * 2024);
+      }
+    }
+    return newStones;
+  }
+
+  private Long[] splitLongDigitsInTwo(Long stone) {
+    String stoneString = stone.toString();
+    int mid = stoneString.length() / 2;
+    String firstHalf = stoneString.substring(0, mid);
+    String secondHalf = stoneString.substring(mid);
+    return new Long[] {Long.parseLong(firstHalf), Long.parseLong(secondHalf)};
+  }
+
+
+  public void print(List<Long> stones) {
+    for (Long stone : stones) {
+      System.out.print(stone + " ");
+    }
+    System.out.println();
+  }
+
+
+  public List<Long> read(String s) throws FileNotFoundException {
     var sc = new Scanner(new File(s));
-    return null;
+    List<Long> result = new ArrayList<>();
+    while (sc.hasNextLong()) {
+      result.add(sc.nextLong());
+    }
+    return result;
   }
 }
--- src/test/java/year2024/Day11/MainTest.java
+++ src/test/java/year2024/Day11/MainTest.java
@@ -10,18 +10,10 @@
     public void part1() throws FileNotFoundException {
       var main = new Main();
       var input = main.read("./src/test/java/year2024/Day11/input");
-      int solution = main.solve_pt1(input);
+      int solution = main.solve(input);
       System.out.println("Solution: " + solution);
-      assert solution == 1;
+      assert solution == 55312;
     }
 
-    @Test
-    public void part2() throws FileNotFoundException {
-      var main = new Main();
-      var input = main.read("./src/test/java/year2024/Day11/input");
-      int solution = main.solve_pt2(input);
-      System.out.println("Solution: " + solution);
-      assert solution == 1;
-    }
   }
 
